5d8a473a04539e46b6c042ca760232c3fd11f815
Salma-Gader <salmagader@gmail.com>
2022-12-25 17:17:38 +0100

    show question and answes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let startButton = document.getElementById("start");
 let randomQuestion, currentQustionIndex;
 const questionElement = document.getElementById("question")
 const answrsElement = document.getElementById("answers-btn")
+const nextButton = document.getElementById("next")
 pageQuestion.style.display = "none";
 //functions***
 function startQuiz() {
@@ -12,19 +13,30 @@ function startQuiz() {
     currentQustionIndex = 0;
     pageQuestion.style.display = "block";
     pageInfo.style.display = "none";
+
     setNextQuetion();
 }
 
 function setNextQuetion() {
+    resetOptions()
     showQuestuion(randomQuestion[currentQustionIndex])
 }
 
 function showQuestuion(question) {
     questionElement.innerText = question.question
-    question.answrs.forEach(answer => {
-        const button = document.createElement('button')
-        button.innerHTML = answer.text
+    question.options.forEach(option => {
+        const button = document.createElement('li')
+        button.innerText = option.text
         button.classList.add('selected')
-
+        if (option.correct) {
+            button.dataset.correct = option.correct
+        }
+        // button.addEventListener('click',selectAnswer)
+        answrsElement.appendChild(button)
     });
+}
+
+function resetOptions() {
+    nextButton.classList.add('hide')
+
 }
